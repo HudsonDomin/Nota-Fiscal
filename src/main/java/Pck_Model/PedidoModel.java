dbb482524f2a424a697933548ffbefb000921d96
Hudson Henrique <hudson.dev.domin@gmail.com>
2025-03-20 16:18:04 -0300

    Update PedidoModel.java

--- src/main/java/Pck_Model/PedidoModel.java
+++ src/main/java/Pck_Model/PedidoModel.java
@@ -9,5 +9,42 @@ package Pck_Model;
  * @author lab03aluno
  */
 public class PedidoModel {
+    private int a02_codigo;
+    private Date a02_data;
+    private float a02_valorTotal;
+    private int a01_codigo;
+
+    public int getA02_codigo() {
+        return a02_codigo;
+    }
+
+    public void setA02_codigo(int a02_codigo) {
+        this.a02_codigo = a02_codigo;
+    }
+
+    public Date getA02_data() {
+        return a02_data;
+    }
+
+    public void setA02_data(Date a02_data) {
+        this.a02_data = a02_data;
+    }
+
+    public float getA02_valorTotal() {
+        return a02_valorTotal;
+    }
+
+    public void setA02_valorTotal(float a02_valorTotal) {
+        this.a02_valorTotal = a02_valorTotal;
+    }
+
+    public int getA01_codigo() {
+        return a01_codigo;
+    }
+
+    public void setA01_codigo(int a01_codigo) {
+        this.a01_codigo = a01_codigo;
+    }
+    
     
 }
