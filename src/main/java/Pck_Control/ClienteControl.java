f2ceffcc26dcc62124bc98603544dc72bdb459b8
Hudson Henrique <hudson.dev.domin@gmail.com>
2025-03-20 16:16:28 -0300

    Update ClienteControl.java

--- src/main/java/Pck_Control/ClienteControl.java
+++ src/main/java/Pck_Control/ClienteControl.java
@@ -4,10 +4,44 @@
  */
 package Pck_Control;
 
+import Pck_Model.ClienteModel;
 /**
  *
  * @author lab03aluno
  */
 public class ClienteControl {
     
+    ClienteModel objClienteModel = new ClienteModel();
+    
+    public void inserirCliente(String sNome, String sEndereco,String sTelefone, String sCpf, float fCredito ){
+        
+        objClienteModel.setA01_nome(sNome);
+        objClienteModel.setA01_endereco(sEndereco);
+        objClienteModel.setA01_telefone(sTelefone);
+        objClienteModel.setA01_cpf(sCpf);
+        objClienteModel.setA01_credito(fCredito);
+        
+    }
+    
+    public void removerCliente(int iCodigo){
+        objClienteModel.setA01_codigo(iCodigo);
+    }
+    
+    public void atualizarCliente(int iCodigo, String sNome, String sEndereco,String sTelefone, String sCpf, float fCredito ){
+        
+        objClienteModel.setA01_codigo(iCodigo);
+        objClienteModel.setA01_nome(sNome);
+        objClienteModel.setA01_endereco(sEndereco);
+        objClienteModel.setA01_telefone(sTelefone);
+        objClienteModel.setA01_cpf(sCpf);
+        objClienteModel.setA01_credito(fCredito);
+        
+    }
+    
+    public void consultarCliente(int iCodigo){
+        
+        objClienteModel.setA01_codigo(iCodigo);
+        
+    }
+  
 }
