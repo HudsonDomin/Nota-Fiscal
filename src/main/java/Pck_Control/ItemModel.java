2afb530fc9bd20aac9dcacfaa1b968aa80ef16be
Hudson Henrique <hudson.dev.domin@gmail.com>
2025-03-27 16:17:13 -0300

    Adição das Classes Model
    Item, Pedidos e Produtos

---
+++ src/main/java/Pck_Control/ItemModel.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pck_Model;
+
+/**
+ *
+ * @author lab03aluno
+ */
+public class ItemModel {   
+     private int a04_codigo;
+     private int a03_codigo;
+     private int a02_codigo;
+     
+     private int a04_quantidade;
+     private int a04_valor_item;
+
+    public int getA04_codigo() {
+        return a04_codigo;
+    }
+
+    public void setA04_codigo(int a04_codigo) {
+        this.a04_codigo = a04_codigo;
+    }
+
+    public int getA03_codigo() {
+        return a03_codigo;
+    }
+
+    public void setA03_codigo(int a03_codigo) {
+        this.a03_codigo = a03_codigo;
+    }
+
+    public int getA02_codigo() {
+        return a02_codigo;
+    }
+
+    public void setA02_codigo(int a02_codigo) {
+        this.a02_codigo = a02_codigo;
+    }
+
+    public int getA04_quantidade() {
+        return a04_quantidade;
+    }
+
+    public void setA04_quantidade(int a04_quantidade) {
+        this.a04_quantidade = a04_quantidade;
+    }
+
+    public int getA04_valor_item() {
+        return a04_valor_item;
+    }
+
+    public void setA04_valor_item(int a04_valor_item) {
+        this.a04_valor_item = a04_valor_item;
+    }
+     
+     
+}
