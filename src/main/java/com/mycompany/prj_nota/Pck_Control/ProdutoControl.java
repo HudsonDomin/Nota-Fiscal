9a07013ffdd4de0aa472cb4cb0ea2563aaea9a0c
Paulo Bispo <61100415+zSNARTdev@users.noreply.github.com>
2025-04-10 15:33:48 -0300

    10/04/2025 - Alteração ID -> InserirProduto
    O parâmetro e o método GET que receberia o ID no método InserirProduto() -> foi colocado como comentário.

--- src/main/java/com/mycompany/prj_nota/Pck_Control/ProdutoControl.java
+++ src/main/java/com/mycompany/prj_nota/Pck_Control/ProdutoControl.java
@@ -5,8 +5,8 @@ import com.mycompany.prj_nota.Pck_Model.ProdutoModel;
 public class ProdutoControl {
     ProdutoModel objProdutoModel = new ProdutoModel();
 
-    public void inserirProduto(int iCodigo, String sDescricao, double dValorUnitario, int iEstoque){
-        objProdutoModel.setA03_codigo(iCodigo);
+    public void inserirProduto(/*int iCodigo,*/ String sDescricao, double dValorUnitario, int iEstoque){
+        /*objProdutoModel.setA03_codigo(iCodigo);*/
         objProdutoModel.setA03_descricao(sDescricao);
         objProdutoModel.setA03_valorUnitario(dValorUnitario);
         objProdutoModel.setA03_estoque(iEstoque);
